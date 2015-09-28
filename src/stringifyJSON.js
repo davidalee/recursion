eff4412d699ce46ec28c5db5749a5e7a97f11057
David Lee <david7lee@gmail.com>
2015-09-28 16:52:54 -0400

    Temporary commit - partially implemented stringifyJSON()

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,41 @@
 
 // but you don't so you're going to write it from scratch:
 
+// JSON.stringify({ x: 5, y: 6 });
+//'{"x":5,"y":6}' or '{"y":6,"x":5}'
+
 var stringifyJSON = function(obj) {
-  // your code goes here
+	console.log(typeof obj, obj);		// for debugging purposes; remove when done
+  if (Array.isArray(obj)) {
+  	if (obj.length === 0) {		// Checks for empty array
+  		return '[]';
+  	}
+
+  	return obj.map(function(x) {
+  		return stringifyJSON(x);
+  	})
+  } else if (typeof obj === 'string') {
+  	return '"' + obj + '"';
+  } else if (obj === null) {
+  	return 'null';
+  } else if (typeof obj === 'boolean') {
+  	return String(obj);
+  } else if (typeof obj === 'number') {
+  	return String(obj);
+  } else if (typeof obj === 'object') {
+
+  	if (Object.keys(obj).length === 0) {		// Checks for empty objects
+  		return 'null';
+  	}
+
+  	var result = [];
+
+  	for (var key in obj) {
+  		result.push('"' + key + '":' + obj[key]);
+  	}
+
+  	return '{' + result.join(',') + '}';
+  } else {
+  	return String(obj);		// returns if obj is a boolean or number
+  }
 };
